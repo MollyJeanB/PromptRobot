7b750c0071c1c964816bf0e00ccbc5e010719c29
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-01-05 14:20:26 -0800

    server update

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ app.get("/words", (req, res) => {
   })
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
